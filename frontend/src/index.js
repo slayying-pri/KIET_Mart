c434097bdb00132ce2134ddc85cc4b601b92c8be
Slayying Pri <priyanshuvashistha001@gmail.com>
2025-09-04 14:19:02 +0530

    import setupFetch configured

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,10 @@
+import './config/setupFetch';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from "react-router-dom";
-import './config/setupFetch';
+
 
 
 
