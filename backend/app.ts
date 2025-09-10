e1e63872b3a4a896da05372fb886bf0f6dee72d5
Slayying Pri <priyanshuvashistha001@gmail.com>
2025-09-10 22:53:56 +0530

    Commiting for backend

--- backend/app.ts
+++ backend/app.ts
@@ -2,7 +2,6 @@
 import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
-import './config/setupFetch';
 
 // Initiating Connection to the MongoDB
 import './src/db/conn';
